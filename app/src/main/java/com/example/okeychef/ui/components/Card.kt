c83db820d0b1dc038b714f28eca1f530cf1a72e0
rfqagst <agustiarifqi@gmail.com>
2024-04-27 09:44:18 +0700

    add Card

--- app/src/main/java/com/example/okeychef/ui/components/Card.kt
+++ app/src/main/java/com/example/okeychef/ui/components/Card.kt
@@ -3,6 +3,7 @@ package com.example.okeychef.ui.components
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -29,7 +30,6 @@ import com.example.okeychef.R
 
 @Composable
 fun BannerImage() {
-
 }
 
 @Composable
@@ -82,33 +82,105 @@ fun CardFood(
 }
 
 
-
 @Composable
-fun CardFilterKategori() {
-
+fun CardFilterKategoriHome(modifier: Modifier, image: Painter, resepName: String) {
+    Box(
+        modifier
+            .border(
+                width = 2.dp,
+                color = Color(0xFFF05252),
+                shape = RoundedCornerShape(size = 10.dp)
+            )
+            .width(147.dp)
+            .height(118.dp),
+        contentAlignment = Alignment.BottomStart
+    ) {
+        Image(painter = image, contentDescription = null, contentScale = ContentScale.Crop)
+        Text(modifier = Modifier.padding(12.dp), text = resepName)
+    }
 }
 
 @Composable
-fun CardFilterKategoriHome() {
-
+fun CardFilterKategori(modifier: Modifier, image: Painter, resepName: String) {
+    Box(
+        modifier
+            .border(
+                width = 2.dp,
+                color = Color(0xFFF05252),
+                shape = RoundedCornerShape(size = 10.dp)
+            )
+            .width(105.dp)
+            .height(60.dp),
+        contentAlignment = Alignment.BottomStart
+    ) {
+        Image(painter = image, contentDescription = null, contentScale = ContentScale.Crop)
+        Text(modifier = Modifier.padding(12.dp), text = resepName)
+    }
 }
 
 @Composable
-fun CardKategori() {
-
+fun CardKategori(
+    modifier: Modifier,
+    image: Painter,
+    foodName: String,
+    foodLocation: String,
+    like: String
+) {
+    Column(
+        modifier = modifier
+            .border(
+                width = 1.dp, color = Color(0xFFE5E7EB),
+                shape = RoundedCornerShape(size = 10.dp)
+            )
+            .width(153.dp)
+            .padding(bottom = 12.dp)
+    ) {
+        Box(
+            modifier = Modifier,
+            contentAlignment = Alignment.TopEnd
+        ) {
+            Image(painter = image, contentDescription = null, contentScale = ContentScale.Crop)
+            Column(Modifier.padding(12.dp)) {
+                Icon(imageVector = Icons.Default.FavoriteBorder, contentDescription = null)
+                Text(text = like)
+            }
+        }
+        Column(Modifier.padding(12.dp)) {
+            Text(text = foodName)
+            Row {
+                Icon(imageVector = Icons.Default.LocationOn, contentDescription = null)
+                Text(text = foodLocation)
+            }
+        }
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun Preview() {
-    CardFood(
-        image = painterResource(id = R.drawable.main_banner_image),
-        foodName = "Nasi Goreng",
+//    CardFood(
+//        image = painterResource(id = R.drawable.main_banner_image),
+//        foodName = "Nasi Goreng",
+//        foodLocation = "Jakarta",
+//        like = "400",
+//        modifier = Modifier
+//            .padding(16.dp)
+//            .width(196.dp)
+//            .height(204.dp)
+//    )
+
+//    CardFilterKategori(
+//        image = painterResource(id = R.drawable.main_banner_image),
+//        resepName = "Tumis",
+//        modifier = Modifier.fillMaxWidth()
+//    )
+
+    CardKategori(
         foodLocation = "Jakarta",
+        foodName = "Sop",
+        image = painterResource(id = R.drawable.main_banner_image),
         like = "400",
-        modifier = Modifier
-            .padding(16.dp)
-            .width(196.dp)
-            .height(204.dp)
+        modifier = Modifier.fillMaxWidth().padding(20.dp)
     )
 }
